7cf644266516812f90d9851e234508766cdb6ede
zhanming.zhang@yatsenglobal.com <zhanming.zhang@yatsenglobal.com>
2019-11-16 16:40:04 +0800

    Initial commit

---
+++ Http/download.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*- 
+'''
+
+ * @version: 0.0.0
+ * @Company: 
+ * @Author: zhanming.zhang 
+ * @Date: 2019/11/16 15:53 
+ * @Last Modified by:   zhanming.zhang 
+ * @Last Modified time: 2019/11/16 15:53
+ * @Desc: 
+'''
+
+from Http import (requests,
+                  logging,
+                  traceback)
+from Http.response import Response
+
+
+class FetchRequest:
+
+
+
+    def fetch(self, request):
+        logging.debug("sadf")
+        logging.debug("%s %s" % (request, request.url))
+        try:
+            proxies = request.meta.get('proxy', None)
+            _resp = requests.request(
+                method=request.method,
+                url=request.url,
+                headers=request.headers,
+                proxies=proxies
+            )
+            resp = Response(url=_resp.url,
+                            request=request,
+                            headers=_resp.headers,
+                            **request.meta)
+            if request.callback:
+                return request.callback(resp)
+            return resp
+        except:
+            traceback.print_stack()
+            logging.error("Fetch Error %s %s" % (request, request.url))
+            logging.debug("Error Reason: "+traceback.format_exc() )
+
+
---
+++ Http/response.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*- 
+'''
+
+ * @version: 0.0.0
+ * @Company: 
+ * @Author: zhanming.zhang 
+ * @Date: 2019/11/16 15:20 
+ * @Last Modified by:   zhanming.zhang 
+ * @Last Modified time: 2019/11/16 15:20
+ * @Desc: 返回的response对象进行包装
+'''
+
+from Http import *
+
+class Response:
+
+
+    def __init__(self, url, request, headers, **meta):
+
+        self.url = url
+        self.__request = request
+        self.headers = headers
+        self.meta = meta
+
+
+    @property
+    def text(self):
+        return self.__request.text
+
+    @property
+    def content(self):
+        return self.__request.content
+
+    @property
+    def status(self):
+        """
+        返回状态吗
+        :return:
+        """
+        return self.__request.status_code
+
+    def req(self):
+        """
+        返回请求对象
+        :return:
+        """
+        return self.__request
+
+    def json(self):
+        """
+        json反序列化
+        :return:
+        """
+        return self.__request.json()
+
+
+    def html(self, encoding="utf8"):
+        """
+        html标签解析
+        :return:
+        """
+        if encoding is None:
+            encode = chardet.detect(self.content)
+            encoding = encode['encoding']
+
+        return lxml.html.fromstring(self.content.decode(encoding, "ignore"))
+
